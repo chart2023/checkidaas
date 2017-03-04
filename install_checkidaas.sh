e11a559b54cd15c5f43be8007e019f224cb6c5df
Apichart <chart2023@users.noreply.github.com>
2017-03-04 15:45:46 +0700

    Update install_checkidaas.sh

--- install_checkidaas.sh
+++ install_checkidaas.sh
@@ -16,8 +16,5 @@ echo "[keyid]" | tee $keyfile
 echo "num_swipe='$num_swipeid'" | tee --append $keyfile
 echo "num_user='$num_userid'" | tee --append $keyfile
 echo "kinect='$kinectid'" | tee --append $keyfile
-echo "exports.TempLower='$TempLower';" | tee --append /home/ubuntu/echonetlite/device.info
-echo "exports.TempSet='$TempSet';" | tee --append /home/ubuntu/echonetlite/device.info
-echo "exports.ipself='$private_echonet';" | tee --append /home/ubuntu/echonetlite/device.info
 echo "FINISH at:" $(date)
 echo "##########FINISHED############"
