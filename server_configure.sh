a386f85ac20a67d50723208989161997a6ef8916
Apichart <chart2023@users.noreply.github.com>
2017-03-14 13:27:17 +0700

    Create server_configure.sh

---
+++ server_configure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "configure"
+echo "START at:" $(date)
+echo "00 01 * * * root /usr/bin/python /home/ubuntu/bem_analytic/querydata.py >> /var/log/fl13data.log" | tee --append /etc/cron.d/cubemswasted
+echo "00 02 * * * root /usr/bin/php /var/www/html/fl13wasted_lab.php >> /var/log/fl13wasted.log" | tee --append /etc/cron.d/cubemswasted
+echo "05 02 * * * root /usr/bin/php /var/www/html/fl13wasted_lecturer.php >> /var/log/fl13wasted.log" | tee --append /etc/cron.d/cubemswasted
+echo "FINISH at:" $(date)
+echo "##########FINISHED############"
