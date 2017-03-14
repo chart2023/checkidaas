1b8f98b6598d6dfeefcbd39d10c8d4fe6adc15f1
Apichart <chart2023@users.noreply.github.com>
2017-03-14 13:35:05 +0700

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -4,5 +4,6 @@ echo "START at:" $(date)
 echo "00 01 * * * root /usr/bin/python /home/ubuntu/bem_analytic/querydata.py >> /var/log/fl13data.log" | tee --append /etc/cron.d/cubemswasted
 echo "00 02 * * * root /usr/bin/php /var/www/html/fl13wasted_lab.php >> /var/log/fl13wasted.log" | tee --append /etc/cron.d/cubemswasted
 echo "05 02 * * * root /usr/bin/php /var/www/html/fl13wasted_lecturer.php >> /var/log/fl13wasted.log" | tee --append /etc/cron.d/cubemswasted
+echo "*/5 * * * * /usr/bin/php /var/www/html/checkidaasfl13.php >> /var/log/checkidaas.log" | tee --append /etc/cron.d/checkidaas
 echo "FINISH at:" $(date)
 echo "##########FINISHED############"
